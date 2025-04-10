6cbd647d49042a3e43913d22f0089ac35a162006
Jalaj-Deep <deepjalaj@gmail.com>
2025-04-10 15:07:57 +0530

    Update payment.controller.js

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -63,7 +63,7 @@ exports.paymentVerification = async (req, res) => {
 
   console.log("Transaction is legit!");
 
-   res.redirect(`https://medicart-frontend.onrender.com`);
+   //res.redirect(`https://medicart-frontend.onrender.com`);
 }catch(err){
   console.log(err);
 }
